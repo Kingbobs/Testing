7c2c6983e714f0ee636ad8c5ab3e56b728587121
KingBob <52259912+Kingbobs@users.noreply.github.com>
2020-05-10 01:24:15 -0600

    Update QuestInfoForm.php

--- src/core/command/forms/QuestInfoForm.php
+++ src/core/command/forms/QuestInfoForm.php
@@ -6,8 +6,8 @@ namespace core\command\forms;
 
 use core\Cryptic;
 use core\CrypticPlayer;
-use dktapps\pmform\CustomForm;
-use dktapps\pmform\element\Label;
+use dktapps\pmforms\CustomForm;
+use dktapps\pmforms\element\Label;
 use pocketmine\Player;
 use pocketmine\utils\TextFormat;
 
