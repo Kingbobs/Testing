6c775c06db1b74947e72c2ea4eb1a5a8f803e39b
MakerbakerYT <65129039+MakerbakerYT@users.noreply.github.com>
2020-05-10 13:30:21 -0600

    Update WithdrawForm.php

--- src/core/command/forms/WithdrawForm.php
+++ src/core/command/forms/WithdrawForm.php
@@ -13,7 +13,6 @@ use core\CrypticPlayer;
 use core\translation\Translation;
 use core\translation\TranslationException;
 use dktapps\pmforms\CustomForm;
-use dktapps\pmforms\BaseForm
 use dktapps\pmforms\CustomFormResponse;
 use dktapps\pmforms\element\Dropdown;
 use dktapps\pmforms\element\Input;
