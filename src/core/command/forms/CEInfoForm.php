f2262f6ca835bdba61385619e4bd48cffe62c6bf
MakerbakerYT <65129039+MakerbakerYT@users.noreply.github.com>
2020-05-10 13:07:47 -0600

    Update CEInfoForm.php

--- src/core/command/forms/CEInfoForm.php
+++ src/core/command/forms/CEInfoForm.php
@@ -6,7 +6,7 @@ namespace core\command\forms;
 
 use core\item\enchantment\Enchantment;
 use core\item\ItemManager;
-use dktapps\pmforms\CustomFormElement;
+use dktapps\pmforms\element\CustomFormElement;
 use dktapps\pmforms\element\Label;
 use pocketmine\utils\TextFormat;
 
